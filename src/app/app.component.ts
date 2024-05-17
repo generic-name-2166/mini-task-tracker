0a7416caefa9e2397dacf027dd01a7d31c7741a0
generic-name-2166 <kirbesson277@mail.ru>
2024-05-17 22:44:26 +0300

    feat: tab navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from "@angular/core";
 import { RouterModule, RouterOutlet } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
+import { MatTabsModule } from "@angular/material/tabs";
+
+interface Link {
+  path: string;
+  name: string;
+}
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [TasksComponent, RouterOutlet, RouterModule],
+  imports: [TasksComponent, MatTabsModule, RouterOutlet, RouterModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = "mini-task-tracker";
+  title = "Mini task tracker";
+  links: Link[] = [
+    { path: "/tasks", name: "Tasks" },
+    { path: "/new", name: "New task" },
+  ];
 }
