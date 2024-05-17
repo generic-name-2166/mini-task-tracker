c5345686d10a803c418e2a247bc6d6a5660f7882
generic-name-2166 <kirbesson277@mail.ru>
2024-05-17 21:13:27 +0300

    feat: saving to localStorage

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from "@angular/core";
 import { TASKS, type Task, Priority, Status } from "./task";
 import { type Observable, of } from "rxjs";
 
+const TASKS_KEY: string = "tasks";
+
 @Injectable({
   providedIn: "root",
 })
 export class TaskService {
+  tasks: Task[] = this.load() ?? TASKS;
+
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return of(this.tasks);
   }
 
   getTaskById(id: number): Observable<Task> | null {
@@ -18,6 +21,25 @@ export class TaskService {
 
   getNewId(): number {
     return TASKS.length;
+  }
+
+  save(): void {
+    if (typeof localStorage === "undefined") {
+      return;
+    }
+    try {
+      localStorage.setItem(TASKS_KEY, JSON.stringify(this.tasks));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  load(): Task[] | null {
+    if (typeof localStorage === "undefined") {
+      return null;
+    }
+    const maybeTasks: string | null = localStorage.getItem(TASKS_KEY);
+    return maybeTasks ? JSON.parse(maybeTasks) : maybeTasks;
   }
 
   addTask(title: string, name: string, due: Date, priority: Priority): void {
@@ -30,7 +52,7 @@ export class TaskService {
       status: Status.NotStarted,
       assignees: [],
     } satisfies Task;
-    console.log(task);
-    // TODO
+    this.tasks = [...this.tasks, task];
+    this.save();
   }
 }
